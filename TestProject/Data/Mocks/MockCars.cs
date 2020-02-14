600ed68ec4deabfeb71db98511c54ee8d172f97d
mivagi <41222007+mivagi@users.noreply.github.com>
2020-02-14 14:56:20 +0300

    К основе добавил MockCars

---
+++ TestProject/Data/Mocks/MockCars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Data.Interfaces;
+using TestProject.Data.Models;
+
+namespace TestProject.Data.Mocks
+{
+    public class MockCars : IAllCars
+    {
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car{Name = "Tesla", Price = 2000000, Img = "/img/tesla.jpg"},
+                    new Car{Name = "Mercedes", Price = 2200000, Img = "/img/mercedes.jpg"}
+                };
+            }
+        }
+    }
+}
