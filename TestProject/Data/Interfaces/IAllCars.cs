a2d52db525f576577111e217e21a1c30de20020e
mivagi <41222007+mivagi@users.noreply.github.com>
2020-02-14 14:41:30 +0300

    Добавьте файлы проекта.

---
+++ TestProject/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Data.Models;
+
+namespace TestProject.Data.Interfaces
+{
+    public interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; }
+    }
+}
---
+++ TestProject/Data/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+    }
+}
